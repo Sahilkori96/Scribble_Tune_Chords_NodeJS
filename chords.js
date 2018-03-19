343a31675101483f8b20603f0a54bc50556aec8e
Sahilkori96 <skori48@gmail.com>
2018-03-19 10:13:48 -0700

    Add All Files

---
+++ chords.js
@@ -0,0 +1,9 @@
+var scribble = require('scribbletune');
+var clip = scribble.clip({
+    notes: 'F#m C#m Dmaj Bm Emaj Amaj Dmaj C#m Amaj',
+    pattern: 'x_x_x_--'.repeat(8),
+    accentMap: 'x---x-x-x',
+    sizzle: true
+});
+
+scribble.midi(clip, 'chords.mid');
